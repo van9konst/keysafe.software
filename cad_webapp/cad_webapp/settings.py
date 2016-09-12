7a8b3cf205ec6a524fd8af42b917cc2518f72ef9
Volodymyr Vozniak <vozniak.vol@hotmail.com>
2016-09-12 09:39:59 +0000

    generate models

--- cad_webapp/cad_webapp/settings.py
+++ cad_webapp/cad_webapp/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'cad_webapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cad_keysafe',
+        'USER': 'cad_root',
+        'PASSWORD': 'root_pass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
