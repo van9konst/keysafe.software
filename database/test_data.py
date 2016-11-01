b2a01097ae8db7edfe35e275013b33687db47eb0
Volodymyr Vozniak <vozniak.vol@hotmail.com>
2016-11-01 18:07:05 +0200

    generate test data

--- database/test_data.py
+++ database/test_data.py
@@ -12,9 +12,9 @@ keys = [{"room": "311", "rfid_chip": "111"},
         {"room": "555", "rfid_chip": "555"},
         {"room": "343", "rfid_chip": "333"}]
 
+
 def create_users():
     for user in users:
-        import pdb;pdb.set_trace()
         User.create(user['firstname'], user['lastname'], user['rfid_card'], user['admin'])
 
     for key in keys:
@@ -25,21 +25,14 @@ create_users()
 user1 = User.get_by_rfid(users[0]['rfid_card'])['data']
 user2 = User.get_by_rfid(users[1]['rfid_card'])['data']
 user3 = User.get_by_rfid(users[2]['rfid_card'])['data']
-user4 = User.get_by_rfid(users[3]['rfid_card'])['data']
-user5 = User.get_by_rfid(users[4]['rfid_card'])['data']
-user6 = User.get_by_rfid(users[5]['rfid_card'])['data']
 key1 = Key.get_by_rfid(keys[0]['rfid_chip'])['data']
 key2 = Key.get_by_rfid(keys[1]['rfid_chip'])['data']
-key3 = Key.get_by_rfid(keys[2]['rfid_chip'])['data']
-key4 = Key.get_by_rfid(keys[3]['rfid_chip'])['data']
-key5 = Key.get_by_rfid(keys[4]['rfid_chip'])['data']
 
 #import pdb;pdb.set_trace()
 print "Start operations with keys"
-UserKeyLink.returning_key(key1.rfid_chip)['data']
 UserKeyLink.getting_key(user1, key1.rfid_chip)['data']
-
 UserKeyLink.getting_key(user2, key2.rfid_chip)['data']
 UserKeyLink.getting_key(user3, key3.rfid_chip)['data']
 UserKeyLink.returning_key(key2.rfid_chip)['data']
+UserKeyLink.returning_key(key1.rfid_chip)['data']
 print "Done"
