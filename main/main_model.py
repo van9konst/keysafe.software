21c71247a3d4a1f9c0c6c3a3103d26782ef4dee8
Volodymyr Vozniak <vozniak.vol@hotmail.com>
2016-11-05 18:43:00 +0200

    added RFID access for users

--- main/main_model.py
+++ main/main_model.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
+import zmq
 from PyQt4 import QtGui, QtCore
 
 keysafe_dir = os.path.expanduser("~/keysafe.software")
 sys.path.append(keysafe_dir)
 
 from main.design import main_design
-from get_key.get_key_model import GetKeyWindow
 from read_card.read_card_model import ReadCardWindow
-from welcome_window.welcome_model import WelcomeWindow
-from database.models import User, Key
+from database.models import User
 from info_window.info_model import InfoWindow
 from new_user.new_user_model import AddNewUser
 
@@ -23,25 +22,10 @@ class MainFirstWindow(QtGui.QMainWindow, main_design.Ui_FirstWindow):
         self.info_error = InfoWindow(label_text=u'Вибачте, сталася помилка, зверніться будь ласка до адміністратора')
 
     def authenticate_user(self):
+        self.startReading()
         self.read_card_window = ReadCardWindow()
         self.read_card_window.show()
-        QtCore.QTimer.singleShot(10000, self.read_card_window.close)
-
-        # TODO: open scanning window for 30 sec
-        # TODO: run scanning script
-        # TODO: close after 30 second \ return bad answer \ get rights
-
-    def welcome_window(self, label_text):
-        self.welcome = WelcomeWindow(label_text)
-        self.welcome.show()
-        QtCore.QTimer.singleShot(2000, self.welcome.close)
-
-    def get_key_window(self, keys, user):
-        self.get_keys_window = GetKeyWindow(keys, user)
-        QtCore.QTimer.singleShot(2000, self.get_keys_window.show) # time when welcome window close and this window open
-
-        # Time to close get_key_window
-        #QtCore.QTimer.singleShot(60000, self.get_keys_window.close)
+        QtCore.QTimer.singleShot(10000, self.read_card_window.read_card_result)
 
     def get_the_keys(self):
         if User.get_all()['warnings']:
@@ -51,24 +35,23 @@ class MainFirstWindow(QtGui.QMainWindow, main_design.Ui_FirstWindow):
             QtCore.QTimer.singleShot(3000, self.info.close)
             QtCore.QTimer.singleShot(3000, self.new_user.show)
         else:
-            # TODO: Open window with user auth
-            user = User.get_by_rfid("12334")['data']
-            #self.authenticate_user()
-            username = user.firstname + u' ' + user.lastname
-            self.welcome_window(username)
-            keys = Key.get_all()
-            if keys['errors']:
-                self.info_error.show()
-                QtCore.QTimer.singleShot(5000, self.info_error.close)
-            elif keys['warnings']:
-                self.get_key_window(keys=None, user=None)
-            else:
-                self.get_key_window(keys['data'], user=user)
-                # TODO: Need return error window
+            self.authenticate_user()
 
     def put_the_keys(self):
         # TODO: Add a window for put keys
         return
+
+    def startReadingRoomCard(self):
+        context = zmq.Context()
+        socket = context.socket(zmq.REQ)
+        socket.connect('tcp://127.0.0.1:5555')
+        socket.send("readRoomId")
+
+    def startReading(self):
+        context = zmq.Context()
+        socket = context.socket(zmq.REQ)
+        socket.connect('tcp://127.0.0.1:5555')
+        socket.send("readTeacherId")
 
 
 def main():
--- read_card/read_card_model.py
+++ read_card/read_card_model.py
@@ -1,9 +1,50 @@
-from PyQt4 import QtGui
+# -*- coding: utf8 -*-
+from PyQt4 import QtGui, QtCore
 
+import zmq
 from design import read_card_design
+from info_window.info_model import InfoWindow
+from welcome_window.welcome_model import WelcomeWindow
+from get_key.get_key_model import GetKeyWindow
+from database.models import User, Key
 
 
 class ReadCardWindow(QtGui.QMainWindow, read_card_design.Ui_ReadKeyWindow):
     def __init__(self):
         super(self.__class__, self).__init__()
         self.setupUi(self)
+        self.info_error = InfoWindow(label_text=u'Вибачте, сталася помилка, зверніться будь ласка до адміністратора')
+
+    def read_card_result(self):
+        context = zmq.Context()
+        socket = context.socket(zmq.REQ)
+        socket.connect('tcp://127.0.0.1:5555')
+        socket.send("getTeacherId")
+        msg = socket.recv()
+        check_user = User.get_by_rfid(msg)
+        if check_user['warnings']:
+            self.close()
+        else:
+            self.login(check_user['data'])
+            self.close()
+
+    def welcome_window(self, label_text):
+        self.welcome = WelcomeWindow(label_text)
+        self.welcome.show()
+        QtCore.QTimer.singleShot(5000, self.welcome.close)
+
+    def get_key_window(self, keys, user):
+        self.get_keys_window = GetKeyWindow(keys, user)
+        QtCore.QTimer.singleShot(5000, self.get_keys_window.show)
+
+    def login(self, user):
+        username = user.firstname + u' ' + user.lastname
+        self.welcome_window(username)
+        keys = Key.get_all()
+        if keys['errors']:
+            self.info_error.show()
+            QtCore.QTimer.singleShot(5000, self.info_error.close)
+        elif keys['warnings']:
+            self.get_key_window(keys=None, user=None)
+        else:
+            self.get_key_window(keys['data'], user=user)
